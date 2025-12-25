7d010da3fe053f2521f51edc54d0b4c45f785b66
saadrizvi09 <clasherizvi@gmail.com>
2025-12-25 05:48:21 +0530

    allowed webhook to bypass security

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)','/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher([
+  '/', 
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+  '/api/webhooks/(.*)',  // Allow all webhook routes
+])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
